b7145873c1d449d32b2a8397410ae254888ddab3
shripandit1986-dotcom <shri.pandit1986@gmail.com>
2026-06-05 15:23:55 +0530

    Update app.py

--- Greater Noida Directory(GND)/app.py
+++ Greater Noida Directory(GND)/app.py
@@ -72,8 +72,8 @@ def contact():
 def gnd():
     return "Greater Noida Directory (GND)"
 
-with app.app_context():
-    db.create_all()
-
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+
     app.run(host="0.0.0.0", port=10000)

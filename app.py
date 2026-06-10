3f6ad892ca32a7cac4ffb534d00c6582bf2654e4
shripandit1986-dotcom <shri.pandit1986@gmail.com>
2026-06-10 15:33:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 app.secret_key = "gnd_super_secret_key_2026"
 
 # --------------DATABASE ----------------
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///gnd.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)

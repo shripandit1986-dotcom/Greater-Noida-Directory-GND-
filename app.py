480df70c6e381841a98ea6d1ad9052644aef60d6
shripandit1986-dotcom <shri.pandit1986@gmail.com>
2026-07-09 17:09:52 +0530

    Add file upload capability with secure filename

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, session
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.utils import secure_filename
 import os
 
 # ---------------- APP ----------------

dd6239f01a5840387a0389f71b8c8e57de397e7b
shripandit1986-dotcom <shri.pandit1986@gmail.com>
2026-07-09 18:57:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,12 @@ import os
 # ---------------- APP ----------------
 app = Flask(__name__)
 app.secret_key = "gnd_super_secret_key_2026"
+cloudinary.config(
+    cloud_name=os.getenv("CLOUDINARY_CLOUD_NAME"),
+    api_key=os.getenv("CLOUDINARY_API_KEY"),
+    api_secret=os.getenv("CLOUDINARY_API_SECRET"),
+    secure=True
+)
 
 # --------------DATABASE ----------------
 import os
